a23795debc24cdcb79ede6ceefa01c9b7facf6af
MHamzaAhmad <48826572+MHamzaAhmad@users.noreply.github.com>
2019-03-25 07:23:41 -0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,92 +1,40 @@
 #include <iostream>
-#include "task2.h"
+#include <string>
+#include "first test.h"
 using namespace std;
-//default constructor
-Myintegerarray :: Myintegerarray()
+
+// initializing with 0
+Point2d::Point2d()
 {
-	array = new int[10];
-	for (int i = 0; i < 10; i++)
+	for (int i = 0; i < 2; i++)
 	{
-		array[i] = 0;
+		p1[i] = 0;
+		p2[i] = 0;
 	}
 }
-// initializing array
-Myintegerarray::Myintegerarray(int x, int y)
+Point2d::Point2d(int x, int y)
 {
-	array = new int[x];
-	for (int i = 0; i < x; i++)
-	{
-		array[i] = 0;
-	}
-
+	p1[0] = x;
+	p1[1] = y;
 }
-
-// setting user values
-void Myintegerarray::setvalue(int x, int y)
+void Point2d::setPoint(int x , int y)
 {
-	array[x] = y;
+	p2[0] = x;
+	p2[1] = y;
+	
 }
-
-Myintegerarray Myintegerarray::add(Myintegerarray* a)
+void Point2d::print()
 {
-	sum = new int[10];
-	for (int i = 0; i < 10 ; i++)
-	{
-		this->array[i] = this->array[i] + a->array[i];
-	}
-	return *this;
+	cout << "(" << p2[0] << "," << p2[1] << ")" << endl;
 }
-
-// finding maximum
-Myintegerarray Myintegerarray :: findMaximum()
+int Point2d::calcuatedistance(const Point2d &o)
 {
-	int max = 0;
-	for (int i = 0; i < 10; i++)
-	{
-		if (i == 0)
-		{
-			max = this->array[i];
-		}
-		else if (this->array[i] >= max)
-		{
-			max = this->array[i];
-		}
-	}
-	cout << "The maximum number is: " << max << endl;
-	return *this;
+	double x1 = p1[0];
+	double x2 = p2[0];
+	double y1 = p1[1];
+	double y2 = p2[1];
+	double firstentity = x2 - x1;
+	double secondentity = y2 - y1;
+	double answer = pow(firstentity, 2) + pow(secondentity, 2);
+	return answer;
 }
-
-//finding maximum
-
-Myintegerarray Myintegerarray:: findMinimum()
-{
-	int min = 0;
-	for (int i = 0; i < 10; i++)
-	{
-		if (i == 0)
-		{
-			min = this->array[i];
-		}
-		else if (this->array[i] <= min)
-		{
-			min = this->array[i];
-		}
-	}
-	cout << "The minimum number is: " << min << endl;
-	return *this;
-}
-
-// Printing function here
-
-void Myintegerarray::print()
-{
-	cout << "The sum of array is: " << endl;
-	for (int i = 0; i < 10; i++)
-	{
-		cout << this->array[i] << " ";
-
-	}
-	cout << endl;
-}
-
-
